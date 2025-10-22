32e99d71895cadec29084a684f662d2baff0dfad
revit3d <tumanezhikov@yandex.ru>
2025-10-22 17:24:04 +0300

    Model 3.8 MSE

--- detection.py
+++ detection.py
@@ -12,8 +12,17 @@ from PIL import Image
 from torch.optim.swa_utils import AveragedModel, update_bn
 from torch.utils.data import Dataset, DataLoader
 from torch.utils.tensorboard import SummaryWriter
-from torchvision.transforms import v2
 from tqdm.auto import tqdm
+
+
+def get_device():
+    if torch.cuda.is_available():
+        device = 'cuda'
+    elif torch.mps.is_available():
+        device = 'mps'
+    else:
+        device = 'cpu'
+    return device
 
 
 def generate_heatmap(size, keypoints, sigma):
@@ -22,8 +31,6 @@ def generate_heatmap(size, keypoints, sigma):
     heatmaps = np.zeros((num_points, h, w), dtype=np.float32)
 
     for i, (x, y) in enumerate(keypoints):
-        # if x < 0 or y < 0 or x >= w or y >= h:
-        #     continue
         xx, yy = np.meshgrid(np.arange(w), np.arange(h))
         heatmaps[i] = np.exp(-((xx - x) ** 2 + (yy - y) ** 2) / (2 * sigma ** 2))
 
@@ -31,11 +38,26 @@ def generate_heatmap(size, keypoints, sigma):
 
 
 class FaceImageDataset(Dataset):
-    def __init__(self, image_dir, gt_dir, img_size, transform: v2.Transform, sigma):
+    def __init__(
+        self,
+        image_dir,
+        img_size,
+        transform: A.Compose,
+        gt=None,
+        sigma=2,
+    ):
         self.image_dir = image_dir
         self.image_files = os.listdir(image_dir)
         self.transform = transform
-        self.targets = pd.read_csv(gt_dir, index_col='filename')
+        if gt is not None:
+            if isinstance(gt, dict):
+                self.targets = pd.DataFrame(gt).T
+            elif isinstance(gt, str):
+                self.targets = pd.read_csv(gt, index_col='filename')
+            else:
+                raise RuntimeError('Invalid format of dataset gt')
+        else:
+            self.targets = None
         self.img_size = img_size
         self.sigma = sigma
 
@@ -46,18 +68,103 @@ class FaceImageDataset(Dataset):
         img_file = self.image_files[idx]
         img_path = os.path.join(self.image_dir, img_file)
         image = np.array(Image.open(img_path).convert('RGB'))
+        img_size = image.shape[:2]
 
-        keypoints = self.targets.loc[img_file].to_numpy().reshape(-1, 2)
-        transformed = self.transform(image=image, keypoints=keypoints)
-        target = transformed['keypoints']
-
-        target = generate_heatmap(self.img_size, target, sigma=self.sigma)
-        return transformed['image'], torch.Tensor(target)
+        target = None
+        if self.targets is not None:
+            keypoints = self.targets.loc[img_file].to_numpy().reshape(-1, 2)
+            transformed = self.transform(image=image, keypoints=keypoints)
+            target = transformed['keypoints']
+            target = generate_heatmap(self.img_size, target, sigma=self.sigma)
+            return transformed['image'], torch.Tensor(target)
+        else:
+            transformed = self.transform(image=image)
+            return transformed['image'], torch.Tensor(img_size)
 
 
 def remap_keypoints(keypoints, **_):
     flip_mapping = [3, 2, 1, 0, 9, 8, 7, 6, 5, 4, 10, 13, 12, 11]
     return keypoints[flip_mapping]
+
+
+def prepare_dataloaders(
+    image_dir,
+    gt=None,
+    img_size=(100, 100),
+    split: tuple[float] | tuple[list[str]] = (0.9, 0.1),
+    batch_size=32,
+    num_workers=0,
+    sigma=2,
+    seed=42,
+    dataset_mean=[0.5364, 0.4303, 0.3750],
+    dataset_std=[0.2378, 0.2182, 0.2084],
+):
+    transforms = A.Compose([
+        A.OneOf([
+            A.Compose([
+                A.HorizontalFlip(p=1.0),
+                A.Lambda(keypoints=remap_keypoints),
+            ], p=0.5),
+            A.NoOp(p=0.5),
+        ]),
+        A.ShiftScaleRotate(
+            shift_limit=0.1, scale_limit=0.1, rotate_limit=20, border_mode=0, p=0.8,
+        ),
+        A.RandomResizedCrop(size=img_size, scale=(0.8, 1), p=1.0),
+
+        A.OneOf([
+            A.ColorJitter(brightness=0.3, contrast=0.3, saturation=0.3, hue=0.1, p=0.8),
+            A.RandomBrightnessContrast(brightness_limit=0.3, contrast_limit=0.3, p=0.8),
+        ], p=0.7),
+        A.GaussianBlur(blur_limit=(3, 7), p=0.3),
+        A.GaussNoise(std_range=(0.03, 0.07), p=0.3),
+        # A.CoarseDropout(max_holes=5, max_height=16, max_width=16, p=0.5),
+
+        A.Normalize(mean=dataset_mean, std=dataset_std),
+        ToTensorV2(),
+    ], keypoint_params=A.KeypointParams(format='xy', remove_invisible=False))
+
+    val_transforms = A.Compose([
+        A.Resize(*img_size),
+        A.Normalize(mean=dataset_mean, std=dataset_std),
+        ToTensorV2(),
+    ], keypoint_params=A.KeypointParams(format='xy', remove_invisible=False))
+
+    dataset = FaceImageDataset(
+        image_dir=image_dir,
+        gt=gt,
+        img_size=img_size,
+        transform=transforms,
+        sigma=sigma,
+    )
+
+    if isinstance(split[0], float):
+        generator = torch.Generator().manual_seed(seed)
+        train_dataset, val_dataset = torch.utils.data.random_split(
+            dataset,
+            split,
+            generator=generator,
+        )
+    else:
+        train_dataset = dataset
+        val_dataset = dataset
+        train_dataset.image_files = split[0]
+        val_dataset.image_files = split[1]
+    val_dataset.transform = val_transforms
+
+    train_loader = DataLoader(
+        train_dataset,
+        batch_size=batch_size,
+        num_workers=num_workers,
+        shuffle=True,
+    )
+    val_loader = DataLoader(
+        val_dataset,
+        batch_size=batch_size,
+        num_workers=num_workers,
+        shuffle=False,
+    )
+    return train_loader, val_loader
 
 
 def visualize(inputs, targets, outputs=None, n=16):
@@ -150,7 +257,6 @@ class UNetBlock(nn.Module):
             nn.BatchNorm2d(out_ch),
             nn.SiLU(),
         )
-        # Add a 1x1 conv if in_ch dimensions don't match for the residual
         if in_ch == out_ch:
             self.residual = nn.Identity()
         else:
@@ -215,7 +321,7 @@ class UNet(nn.Module):
         d1 = self.dec1(d1)
 
         out = self.final(d1)
-        return self.sigmoid(out)  # (B, num_landmarks, H, W)
+        return self.sigmoid(out)
 
 
 def weighted_mse_loss(pred, target, weight_factor=10.0):
@@ -237,7 +343,6 @@ class Trainer:
         device: str | torch.device = 'cpu',
     ):
         self.model = model
-        # self.ema_model = swa_utils.AveragedModel(model, avg_fn=lambda avg, new, n: 0.999 * avg + 0.001 * new)
         self.criterion = criterion
         self.optimizer = optimizer
         self.scheduler = scheduler
@@ -245,25 +350,20 @@ class Trainer:
         self.swa_model = swa_model
         self.device = device
 
-    def step(self, inputs, targets, model=None) -> torch.Tensor:
-        if model is None:
-            model = self.model
+    def step(self, inputs, targets) -> torch.Tensor:
         inputs = inputs.to(self.device)
         targets = targets.to(self.device)
-        output = model(inputs)
+        output = self.model(inputs)
         loss = self.criterion(output, targets)
         return loss, output
 
     @torch.no_grad
-    def validate(self, loader, step, ema=False):
-        if ema:
-            self.ema_model.module.eval()
-        else:
-            self.model.eval()
+    def validate(self, loader, step):
+        self.model.eval()
 
         loss_total = 0
         for inputs, targets in loader:
-            _, output = self.step(inputs, targets, self.ema_model.module if ema else None)
+            _, output = self.step(inputs, targets)
             target_coo = heatmaps_to_coords(targets.cpu())
             output_coo = heatmaps_to_coords(output.cpu())
             loss_total += nn.functional.mse_loss(target_coo, output_coo)
@@ -271,21 +371,21 @@ class Trainer:
         loss_total = loss_total.cpu().item()
 
         if self.logger:
-            self.logger.add_scalar(f'Loss/Validation{'_ema' if ema else ''}', loss_total, global_step=step)
+            self.logger.add_scalar('Loss/Validation', loss_total, global_step=step)
 
-        if ema:
-            self.ema_model.module.train()
-        else:
-            self.model.train()
-
+        self.model.train()
         return loss_total
 
-    def train(self, train_loader: DataLoader, val_loader: DataLoader, n_epochs: int):
+    def train(
+        self,
+        train_loader: DataLoader,
+        val_loader: DataLoader,
+        n_epochs: int,
+        fast_train: bool = False,
+    ):
         self.model.train()
-        # self.ema_model.module.train()
 
         val_losses = []
-        ema_val_losses = []
         for epoch in tqdm(range(n_epochs)):
             last_output = None
             for i, (inputs, targets) in enumerate(train_loader):
@@ -294,16 +394,19 @@ class Trainer:
                 loss.backward()
                 #torch.nn.utils.clip_grad_norm_(self.model.parameters(), max_norm=5.0)
                 self.optimizer.step()
-                # self.ema_model.update_parameters(self.model)
                 if self.logger:
                     global_step = epoch * len(train_loader) + i
                     self.logger.add_scalar(
                         'Loss/Train', loss.item(), global_step,
                     )
                     if self.scheduler:
-                        self.logger.add_scalar("Scheduler/lr", self.scheduler.get_last_lr()[0], global_step)
+                        self.logger.add_scalar(
+                            'Scheduler/lr', self.scheduler.get_last_lr()[0], global_step,
+                        )
                 if self.scheduler:
                     self.scheduler.step()
+                if fast_train and i > 1:
+                    break
             if self.logger:
                 target_coo = heatmaps_to_coords(targets.cpu())
                 output_coo = heatmaps_to_coords(last_output.detach().cpu())
@@ -314,15 +417,11 @@ class Trainer:
             if self.swa_model:
                 self.swa_model.update_parameters(self.model)
 
-            val_loss = self.validate(val_loader, step=epoch, ema=False)
-            # ema_val_loss = self.validate(val_loader, step=epoch, ema=True)
+            val_loss = self.validate(val_loader, step=epoch)
             val_losses.append(val_loss)
-            # ema_val_losses.append(ema_val_loss)
 
         self.model.eval()
-        # self.ema_model.module.eval()
-
-        return val_losses, ema_val_losses
+        return val_losses
 
 
 def detect(model_path: str, images_path: str) -> dict:
@@ -330,13 +429,51 @@ def detect(model_path: str, images_path: str) -> dict:
     Load model from `model_path` and make predictions for all images
     in `images_path` in format [image_file] -> [x1, y1, ..., x14, y14].
     """
-    pass
+    device = 'cpu'
+
+    model = UNet()  #AveragedModel(UNet())
+    model.load_state_dict(torch.load(model_path, weights_only=True))
+    model = model.to(device)
+    model.eval()
+
+    img_size = (100, 100)
+    dataset_mean = [0.5364, 0.4303, 0.3750]
+    dataset_std = [0.2378, 0.2182, 0.2084]
+    transforms = A.Compose([
+        A.Resize(*img_size),
+        A.Normalize(mean=dataset_mean, std=dataset_std),
+        ToTensorV2(),
+    ])
+
+    dataset = FaceImageDataset(
+        image_dir=images_path,
+        img_size=img_size,
+        transform=transforms,
+    )
+
+    loader = DataLoader(
+        dataset,
+        batch_size=16,
+        num_workers=0,
+        shuffle=False,
+    )
+
+    predictions = {}
+    files = iter(dataset.image_files)
+    for inputs, img_sizes in loader:
+        inputs = inputs.to(device)
+        output = model(inputs)
+        output_coo = heatmaps_to_coords(output.cpu())
+        output_coo *= img_sizes[:, None, :] / 100
+        for coo in output_coo:
+            file_name = next(files)
+            predictions[file_name] = coo.view(-1).round().tolist()
+    return predictions
 
 
 def train_detector(
-    train_set: list[str],
-    val_set: list[str],
-    images_path: str,
+    train_gt,
+    train_img_dir,
     fast_train: bool = False,
 ) -> torch.nn.Module:
     """
@@ -344,4 +481,58 @@ def train_detector(
     If `fast_train` is set to `True`, this function runs on cpu,
     ignores logging, uses one thread and makes significantly less train steps.
     """
-    pass
+    device = 'cpu' if fast_train else get_device()
+
+    hparams = {
+        'sigma': 2,
+        'optimizer': 'AdamW',
+        'lr': 5e-4,
+        'weight_decay': 5e-4,
+        'batch_size': 32,
+        'img_size': '100x100',
+        'scheduler': 'OneCycleLR',
+        'max_lr': 5e-3,
+        'ch_mul': 32,
+        'grad_clip': None,
+        'epochs': 1 if fast_train else 50,
+        'log_dir': './logs/exp_model_heatmap4'
+    }
+
+    train_loader, val_loader = prepare_dataloaders(
+        image_dir=train_img_dir,
+        gt=train_gt,
+        img_size=tuple(map(int, hparams['img_size'].split('x'))),
+        split=(0.9, 0.1),
+        batch_size=hparams['batch_size'],
+        num_workers=0 if fast_train else 8,
+        sigma=hparams['sigma'],
+    )
+
+    model = UNet(ch_mul=hparams['ch_mul'])
+    optimizer = torch.optim.AdamW(
+        model.parameters(),
+        lr=hparams['lr'],
+        weight_decay=hparams['weight_decay'],
+    )
+    scheduler = torch.optim.lr_scheduler.OneCycleLR(
+        optimizer,
+        max_lr=hparams['max_lr'],
+        steps_per_epoch=len(train_loader),
+        epochs=hparams['epochs'],
+    )
+    logger = None if fast_train else SummaryWriter(log_dir=hparams['log_dir'])
+    trainer = Trainer(
+        model=model.to(device),
+        criterion=weighted_mse_loss,
+        optimizer=optimizer,
+        scheduler=scheduler,
+        logger=logger,
+        device=device,
+    )
+    val_losses = trainer.train(
+        train_loader, val_loader, n_epochs=hparams['epochs'], fast_train=fast_train,
+    )
+    if logger:
+        logger.add_hparams(hparams, {'val_loss': val_losses[-1]})
+        logger.close()
+    return trainer.model.cpu()
